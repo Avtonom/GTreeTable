d4f8f9190617f6e0570b1d0a12225fdd03750575
Anton Utkin <avtonomspb@gmail.com>
2014-05-29 12:10:37 +0400

    add languages RU

---
+++ languages/bootstrap-gtreetable.ru.js
@@ -0,0 +1,10 @@
+(function( $ ) {
+    $.fn.gtreetable.defaults.languages.ru = {
+        save: 'Сохранить',
+        cancel: 'Отменить',
+        action: 'Действия',
+        actionAdd: 'Добавить',
+        actionEdit: 'Редактировать',
+        actionDelete: 'Удалить'
+    };
+}( jQuery ));
