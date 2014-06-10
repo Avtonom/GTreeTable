89365b32dbcda38492f539258baca176c99758cb
Anton Utkin <avtonomspb@gmail.com>
2014-06-10 11:51:09 +0400

    add RUS language

--- languages/bootstrap-gtreetable.ru.js
+++ languages/bootstrap-gtreetable.ru.js
@@ -1,10 +1,31 @@
-(function( $ ) {
+/* =========================================================
+ * GTreeTableEx
+ * bootstrap-gtreetable.ru.js
+ * =========================================================
+ * Copyright 2014 avtonomspb@mail.ru
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================= */
+
+ (function( $ ) {
     $.fn.gtreetable.defaults.languages.ru = {
         save: 'Сохранить',
         cancel: 'Отменить',
         action: 'Действия',
         actionAdd: 'Добавить',
         actionEdit: 'Редактировать',
-        actionDelete: 'Удалить'
+        actionDelete: 'Удалить',
+        parent_id: 'ID родительского элемента',
+        actionMove: 'Перенести ветку'
     };
 }( jQuery ));
